dc95011a197173394b2d4cc1f5d5b35843d12e2d
Damien Hampton <damien.hampton@gmail.com>
2019-11-01 23:31:39 +0000

    chapters 20 done

--- test/test.py
+++ test/test.py
@@ -4,30 +4,29 @@ class TestCase:
     self.wasSetUp= None
   def setUp(self):
     pass
+  def tearDown(self):
+    pass
   def run(self):
     self.setUp()
     method= getattr(self, self.name)
     method()
+    self.tearDown()
 
 class WasRun(TestCase):
   def __init__(self, name):
-    self.wasRun= None
     TestCase.__init__(self, name)
   def setUp(self):
-    self.wasSetUp= 1
+    self.log= 'setUp '
+  def tearDown(self):
+    self.log+= 'tearDown '
   def testMethod(self):
-    self.wasRun= 1
+    self.log+= 'testMethod '
 
 class TestCaseTest(TestCase):
   def setUp(self):
     self.test= WasRun('testMethod')
-  def testRunning(self):
-    assert(not self.test.wasRun)
+  def testTemplateMethod(self):
     self.test.run()
-    assert(self.test.wasRun)
-  def testSetUp(self):
-    self.test.run()
-    assert(self.test.wasSetUp)
+    assert('setUp testMethod tearDown ' == self.test.log)
 
-print(TestCaseTest('testRunning').run())
-print(TestCaseTest('testSetUp').run())
+print(TestCaseTest('testTemplateMethod').run())
