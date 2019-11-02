008738a7c318b67cdda903bb027fe6b1cf474687
Damien Hampton <damien.hampton@gmail.com>
2019-11-02 19:56:42 +0000

    chapters 21 + 22 done

--- test/test.py
+++ test/test.py
@@ -7,10 +7,27 @@ class TestCase:
   def tearDown(self):
     pass
   def run(self):
+    result= TestResult()
+    result.testStarted()
     self.setUp()
-    method= getattr(self, self.name)
-    method()
+    try:
+      method= getattr(self, self.name)
+      method()
+    except:
+      result.testFailed()
     self.tearDown()
+    return result
+
+class TestResult:
+  def __init__(self):
+    self.runCount= 0
+    self.errorCount= 0
+  def testStarted(self):
+    self.runCount+= 1
+  def testFailed(self):
+    self.errorCount+= 1
+  def summary(self):
+    return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class WasRun(TestCase):
   def __init__(self, name):
@@ -21,6 +38,8 @@ class WasRun(TestCase):
     self.log+= 'tearDown '
   def testMethod(self):
     self.log+= 'testMethod '
+  def testBrokenMethod(self):
+    raise Exception
 
 class TestCaseTest(TestCase):
   def setUp(self):
@@ -28,5 +47,20 @@ class TestCaseTest(TestCase):
   def testTemplateMethod(self):
     self.test.run()
     assert('setUp testMethod tearDown ' == self.test.log)
+  def testResult(self):
+    result= self.test.run()
+    assert('1 run, 0 failed' == result.summary())
+  def testFailedResult(self):
+    test = WasRun('testBrokenMethod')
+    result= test.run()
+    assert('1 run, 1 failed' == result.summary())
+  def testFailedResultFormatting(self):
+    result= TestResult()
+    result.testStarted()
+    result.testFailed()
+    assert('1 run, 1 failed' == result.summary())
 
-print(TestCaseTest('testTemplateMethod').run())
+print(TestCaseTest('testTemplateMethod').run().summary())
+print(TestCaseTest('testResult').run().summary())
+print(TestCaseTest('testFailedResult').run().summary())
+print(TestCaseTest('testFailedResultFormatting').run().summary())
